98cc45d35da146881cb1d0efe22d5d8abf23d401
Nicolas Aguilar <aguilar-nicolas@javeriana.edu.co>
2024-10-21 19:32:11 -0500

    header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ActivePropertiesComponent } from './components/property/active-properties/active-properties.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ActivePropertiesComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  // Cambiado a 'styleUrls'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'rentyourproperty-frontend';
